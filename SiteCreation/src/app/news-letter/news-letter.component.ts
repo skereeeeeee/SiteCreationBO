14f457c5add90d0c1f8a805a09e85bee74d5e6fa
skereeeeeee <128793380+skereeeeeee@users.noreply.github.com>
2024-07-15 23:36:44 +0200

    fdssfddf

--- SiteCreation/src/app/news-letter/news-letter.component.ts
+++ SiteCreation/src/app/news-letter/news-letter.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
-
+import { NewsLetterServiceService } from '../services/news-letter-service.service';
 
 @Component({
   selector: 'app-news-letter',
   templateUrl: './news-letter.component.html',
-  styleUrls: ['./news-letter.component.css']
+  styleUrls: ['./news-letter.component.css'],
 })
 export class NewsLetterComponent implements OnInit {
-
-  constructor() { }
+  constructor(private newsLetterService: NewsLetterServiceService) {}
   public Editor: any = ClassicEditor;
+  messaggio: string = '';
+  vediUtenti: boolean = false;
 
-  ngOnInit(): void {
+  newsLetter: Array<string> = [];
+
+  async ngOnInit(): Promise<void> {
+    this.newsLetter  = (await this.newsLetterService.getUsers()).map(
+      (x) => x.email
+    );
   }
 
+  send() {
+    var editor = this.Editor;
+    debugger;
+    console.log(this.messaggio);
+  }
 }
